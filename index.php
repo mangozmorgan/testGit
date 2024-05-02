0015b5eb0ddd244294be599e285e1b997ca249d7
Bptiste2003 <babatibenj@gmail.com>
2024-05-02 16:43:14 +0200

    Suppression du H1 et ajout de la structure de base de l'HTML5, avec un paragraphe dans la balise body.

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<h1 style="color: pink;">Salut les gens !</h1>
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Page de Test</title>
+    </head>
+
+    <body>
+        <p>Ceci est paragraphe.</p>
+    </body>
+</html>
